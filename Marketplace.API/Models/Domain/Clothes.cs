db6027f1cf204e12be5e797c94060b578fd8b500
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2023-07-31 22:26:38 +0200

    Add project files.

---
+++ Marketplace.API/Models/Domain/Clothes.cs
@@ -0,0 +1,19 @@
+﻿namespace Marketplace.API.Models.Domain {
+	public enum ClothesSize {
+		S,
+		M,
+		L,
+		XL,
+		XXL
+	}
+	public enum Gender {
+		Male, 
+		Female,
+		Both
+	}
+	public class Clothes : GeneralListing {
+		public ClothesSize Size { get; set; }
+		public Gender Gender { get; set; }
+		public string RandomProperty { get; set; }
+	}
+}
---
+++ Marketplace.API/Program.cs
@@ -0,0 +1,30 @@
+using Marketplace.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MarketplaceDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("MarketplaceConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
