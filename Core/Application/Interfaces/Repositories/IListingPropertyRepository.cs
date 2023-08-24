638c281ba5e137a8727d8460f9e58fe40081f5cc
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2023-08-25 00:19:07 +0200

    added repositories for listing properties

---
+++ Core/Application/Interfaces/Repositories/IListingPropertyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+using Domain.Entities.Listings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories {
+	public interface IListingPropertyRepository {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T">T has to be a class/ref type, so that we can add Include() on it in the implementation</typeparam>
+		/// <returns></returns>
+		public Task<List<T>> GetAllAsync<T>() where T : class, IEntity;
+		public Task<T?> GetByIdAsync<T>(Guid id) where T : class, IEntity;
+	}
+}
