2d8aa2632192c12ff8cea6279e1b3a8326c4706d
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2024-09-17 22:43:52 +0200

    Added main menu

--- Frontend/src/src/app/layout.tsx
+++ Frontend/src/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './globals.css';
+import './globals.css';
 import { Inter } from 'next/font/google';
+import TopMenu from './ui/TopMenu';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TopMenu />
+        {children}
+      </body>
     </html>
   );
 }
