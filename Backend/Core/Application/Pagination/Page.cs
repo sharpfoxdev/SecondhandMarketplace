25806df451823ef19a1bf9f8cedd689d035848cc
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2025-07-04 01:02:34 +0200

    add paging on backend

---
+++ Backend/Core/Application/Pagination/Page.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Pagination
+{
+    public class Page<T>
+    {
+        public List<T> Items { get; }
+        public int TotalCount { get; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public Page(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = count;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+
+}
